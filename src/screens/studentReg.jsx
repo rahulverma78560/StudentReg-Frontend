3e8807762a4ee57e64d47451e4b07084ca9eac26
Rahul Kumar <rahul.kumar@g7cr.in>
2023-02-13 00:02:26 +0530

    feat(responsive form)

--- src/screens/studentReg.jsx
+++ src/screens/studentReg.jsx
@@ -135,178 +135,214 @@ const StudentReg = () => {
         </h1>
         <FormContainer>
           <form>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <TextField
-                required
-                error={validFirstName}
-                helperText={validFirstName ? "Please enter first name" : ""}
-                sx={{ m: 2, width: "60vh" }}
-                label='First Name'
-                onChange={(e) => {
-                  setFirstName(e.target.value);
-                  if (e.target.value.trim()) {
-                    setValidFirstName(false);
-                  } else {
-                    setValidFirstName(true);
+            <Grid
+              container
+              rowSpacing={2}
+              columnSpacing={{ xs: 3, sm: 3, md: 3 }}
+              sx={{ m: 2 }}
+            >
+              <Grid item xs={12} sm={4} md={4}>
+                <TextField
+                  fullWidth
+                  required
+                  error={validFirstName}
+                  helperText={validFirstName ? "Please enter first name" : ""}
+                  label='First Name'
+                  onChange={(e) => {
+                    setFirstName(e.target.value);
+                    if (e.target.value.trim()) {
+                      setValidFirstName(false);
+                    } else {
+                      setValidFirstName(true);
+                    }
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={4} md={4}>
+                <TextField
+                  fullWidth
+                  label='Middle Name'
+                  onChange={(e) => {
+                    setMiddleName(e.target.value);
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={4} md={4}>
+                <TextField
+                  fullWidth
+                  required
+                  error={validLastName}
+                  helperText={validLastName ? "Please enter last name" : ""}
+                  label='Last Name'
+                  onChange={(e) => {
+                    setLastName(e.target.value);
+                    if (e.target.value.trim()) {
+                      setValidLastName(false);
+                    } else {
+                      setValidLastName(true);
+                    }
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              rowSpacing={2}
+              columnSpacing={{ xs: 3, sm: 6, md: 6 }}
+              sx={{ m: 2 }}
+            >
+              <Grid item xs={12} sm={6} md={6}>
+                <TextField
+                  fullWidth
+                  required
+                  label='Phone Number'
+                  error={validPhoneNumber}
+                  helperText={
+                    validPhoneNumber ? "Please enter a valid phone number" : ""
                   }
-                }}
-              />
-              <TextField
-                sx={{ m: 2, width: "60vh" }}
-                label='Middle Name'
-                onChange={(e) => {
-                  setMiddleName(e.target.value);
-                }}
-              />
-              <TextField
-                required
-                sx={{ m: 2, width: "60vh" }}
-                error={validLastName}
-                helperText={validLastName ? "Please enter last name" : ""}
-                label='Last Name'
-                onChange={(e) => {
-                  setLastName(e.target.value);
-                  if (e.target.value.trim()) {
-                    setValidLastName(false);
-                  } else {
-                    setValidLastName(true);
-                  }
-                }}
-              />
-            </div>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <TextField
-                required
-                sx={{ m: 2, width: "80vh" }}
-                label='Phone Number'
-                error={validPhoneNumber}
-                helperText={
-                  validPhoneNumber ? "Please enter a valid phone number" : ""
-                }
-                onChange={(e) => {
-                  setPhoneNumber(e.target.value);
-                  if (e.target.value.trim()) {
-                    e.target.value.trim().length < 10
-                      ? setValidPhoneNumber(true)
-                      : setValidPhoneNumber(false);
-                    if (isNaN(Number(e.target.value.trim()))) {
+                  onChange={(e) => {
+                    setPhoneNumber(e.target.value);
+                    if (e.target.value.trim()) {
+                      e.target.value.trim().length < 10
+                        ? setValidPhoneNumber(true)
+                        : setValidPhoneNumber(false);
+                      if (isNaN(Number(e.target.value.trim()))) {
+                        setValidPhoneNumber(true);
+                      }
+                    } else {
                       setValidPhoneNumber(true);
                     }
-                  } else {
-                    setValidPhoneNumber(true);
-                  }
-                }}
-              />
-              <TextField
-                required
-                error={validEmail}
-                helperText={validEmail ? "Please enter a valid Email" : ""}
-                type='email'
-                sx={{ m: 2, width: "80vh" }}
-                label='Email'
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                  if (e.target.value.trim()) {
-                    new RegExp(
-                      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-                    ).test(e.target.value.trim())
-                      ? setValidEmail(false)
-                      : setValidEmail(true);
-                  } else {
-                    setValidEmail(true);
-                  }
-                }}
-              />
-            </div>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <TextField
-                sx={{ m: 2, width: "80vh" }}
-                label='Registration Number'
-                onChange={(e) => {
-                  setRegNumber(e.target.value);
-                }}
-              />
-              <TextField
-                required
-                id='date'
-                error={validRegDate}
-                label='Registration Date'
-                helperText={validRegDate ? "Please provide valid Date" : ""}
-                type='date'
-                sx={{ m: 2, width: "80vh" }}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                onChange={(e) => {
-                  setRegDate(e.target.value);
-                  if (e.target.value.trim()) {
-                    setValidRegDate(false);
-                  } else {
-                    setValidRegDate(true);
-                  }
-                }}
-              />
-            </div>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <FormControl
-                required
-                sx={{ m: 2, width: "60vh" }}
-                error={validGender}
-              >
-                <InputLabel id='demo-simple-select-helper-label'>
-                  Gender
-                </InputLabel>
-                <Select
-                  labelId='demo-simple-select-helper-label'
-                  value={gender}
-                  label='Gender'
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={6}>
+                <TextField
+                  fullWidth
+                  required
+                  error={validEmail}
+                  helperText={validEmail ? "Please enter a valid Email" : ""}
+                  type='email'
+                  label='Email'
                   onChange={(e) => {
-                    setGender(e.target.value);
-                    if (e.target.value) {
-                      setValidGender(false);
+                    setEmail(e.target.value);
+                    if (e.target.value.trim()) {
+                      new RegExp(
+                        /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+                      ).test(e.target.value.trim())
+                        ? setValidEmail(false)
+                        : setValidEmail(true);
                     } else {
-                      setValidGender(true);
+                      setValidEmail(true);
                     }
                   }}
-                >
-                  <MenuItem value={"Male"}>Male</MenuItem>
-                  <MenuItem value={"Female"}>Female</MenuItem>
-                  <MenuItem value={"Transgender"}>Transgender</MenuItem>
-                </Select>
-                <FormHelperText>
-                  {validGender ? "Please select gender" : ""}
-                </FormHelperText>
-              </FormControl>
-              <FormControl sx={{ m: 2, width: "60vh" }}>
-                <InputLabel id='demo-simple-select-helper-label'>
-                  Nationality
-                </InputLabel>
-                <Select
-                  labelId='demo-simple-select-helper-label'
-                  value={nationality}
-                  label='Nationality'
+                />
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              rowSpacing={2}
+              columnSpacing={{ xs: 3, sm: 6, md: 6 }}
+              sx={{ m: 2 }}
+            >
+              <Grid item xs={12} sm={6} md={6}>
+                <TextField
+                  fullWidth
+                  label='Registration Number'
                   onChange={(e) => {
-                    setNationality(e.target.value);
+                    setRegNumber(e.target.value);
                   }}
-                >
-                  <MenuItem value={"Indian"}>Indian</MenuItem>
-                  <MenuItem value={"American"}>American</MenuItem>
-                  <MenuItem value={"German"}>German</MenuItem>
-                </Select>
-              </FormControl>
-              <TextField
-                id='date'
-                label='Date of Birth'
-                type='date'
-                sx={{ m: 2, width: "80vh" }}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                onChange={(e) => {
-                  setDob(e.target.value);
-                }}
-              />
-            </div>
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={6}>
+                <TextField
+                  fullWidth
+                  required
+                  id='date'
+                  error={validRegDate}
+                  label='Registration Date'
+                  helperText={validRegDate ? "Please provide valid Date" : ""}
+                  type='date'
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  onChange={(e) => {
+                    setRegDate(e.target.value);
+                    if (e.target.value.trim()) {
+                      setValidRegDate(false);
+                    } else {
+                      setValidRegDate(true);
+                    }
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              rowSpacing={2}
+              columnSpacing={{ xs: 3, sm: 3, md: 3 }}
+              sx={{ m: 2 }}
+            >
+              <Grid item xs={12} sm={4} md={4}>
+                <FormControl required fullWidth error={validGender}>
+                  <InputLabel id='demo-simple-select-helper-label'>
+                    Gender
+                  </InputLabel>
+                  <Select
+                    labelId='demo-simple-select-helper-label'
+                    value={gender}
+                    label='Gender'
+                    onChange={(e) => {
+                      setGender(e.target.value);
+                      if (e.target.value) {
+                        setValidGender(false);
+                      } else {
+                        setValidGender(true);
+                      }
+                    }}
+                  >
+                    <MenuItem value={"Male"}>Male</MenuItem>
+                    <MenuItem value={"Female"}>Female</MenuItem>
+                    <MenuItem value={"Transgender"}>Transgender</MenuItem>
+                  </Select>
+                  <FormHelperText>
+                    {validGender ? "Please select gender" : ""}
+                  </FormHelperText>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={4} md={4}>
+                <FormControl fullWidth>
+                  <InputLabel id='demo-simple-select-helper-label'>
+                    Nationality
+                  </InputLabel>
+                  <Select
+                    labelId='demo-simple-select-helper-label'
+                    value={nationality}
+                    label='Nationality'
+                    onChange={(e) => {
+                      setNationality(e.target.value);
+                    }}
+                  >
+                    <MenuItem value={"Indian"}>Indian</MenuItem>
+                    <MenuItem value={"American"}>American</MenuItem>
+                    <MenuItem value={"German"}>German</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={4} md={4}>
+                <TextField
+                  id='date'
+                  label='Date of Birth'
+                  type='date'
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  onChange={(e) => {
+                    setDob(e.target.value);
+                  }}
+                />
+              </Grid>
+            </Grid>
             <div style={{ display: "flex", justifyContent: "center" }}>
               <LoadingButton
                 style={!loading ? { backgroundColor: "#370041 " } : {}}
